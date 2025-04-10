3aec7e9dcf0478255876621f343882abd87e892f
jose-manuel17 <cruzperezjose@cbtis116.edu.mx>
2025-04-10 14:32:57 -0700

    first commit}

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import SearchBar from "./assets/Components/SearchBar"
+import ImageList from "./assets/Components/ImageList"
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>app</h1>
+      <SearchBar/>
+      <ImageList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Imagen.List.jsx
@@ -0,0 +1,15 @@
+import ImageShow from "./ImageShow";
+import './ImageList.css'
+
+const ImageList = ({images}) => {
+    const renderImage = images.map((image) => {
+        return <ImageShow key={image.id} image={image} />
+    })
+    return (
+        <div className="Image-list">
+            {renderImage}
+        </div>
+    )
+}
+
+export default ImageList
