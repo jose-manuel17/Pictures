a8105a62a2e2b161b9280f6f19d5389e3e61215d
jose-manuel17 <cruzperezjose@cbtis116.edu.mx>
2025-04-10 18:59:27 -0700

    Se hicieron cambios

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const searchImages = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=ODZvhKORnTpWzXC0AA_2NNTaa-LM9FE-n3JHD_T9kbs'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=ttEeylI51PVEaoeKW5tQlvTi6RjpIujASS1FIbXmwe8'
 
     const response = await axios.get(url,{
         params:{
